0e7374b528458ec68838a9797f6822850d6f9621
ewerton11 <ewertonreisc0@gmail.com>
2023-03-01 11:29:29 -0300

    organizing names, prices, promotions that come from the api

---
+++ src/pages/header/index.tsx
@@ -0,0 +1,9 @@
+import { HeaderStyled, Logo } from '@/styles/header/styled'
+
+export default function Header() {
+  return (
+    <HeaderStyled>
+      <Logo>ewt</Logo>
+    </HeaderStyled>
+  )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import { Feed } from './feed'
+import Header from './header'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>sneakerhead</title>
+        <meta name="description" content="description" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Shadows+Into+Light&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Header />
+      <Feed />
+    </>
+  )
+}
